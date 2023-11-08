688682eebb15d450f1ce057131bf314950d20de3
Rohan-Vetale <60183607+Rohan-Vetale@users.noreply.github.com>
2023-11-08 23:54:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import json
+from llamaapi import LlamaAPI
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+@app.get('/fetLama')
+
+
+async def fetLama(prompt1: str):
+    # Initialize the llamaapi with your api_token
+    llama = LlamaAPI("LL-2FxqtbBbbOS82FwtvY0awiWv7jTnr1BXkFjDkfZTZEluJRJUZgZSCfK6gtCebXig")
+
+    # Define your API request
+    api_request_json = {
+        "messages": [
+            {"role": "user", "content": prompt1},
+        ],
+        
+        "stream": False,
+        
+    }
+
+    # Make your request and handle the response
+    response = llama.run(api_request_json)
+    print(json.dumps(response.json(), indent=2))
+
+    return {
+        json.dumps(response.json(), indent=2)
+    }
+
+
