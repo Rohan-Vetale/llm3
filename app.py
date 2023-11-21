225a0dc79898f488568f29343f4932fda6d5e3c0
Rohan-Vetale <60183607+Rohan-Vetale@users.noreply.github.com>
2023-11-21 10:47:03 +0530

    Update app.py All revert to working

--- app.py
+++ app.py
@@ -5,28 +5,34 @@ from fastapi import FastAPI, Request
 
 
 app = FastAPI()
-@app.post('/fetLama')
+@app.get('/fetLama')
 
 
-async def fetLama(request: Request):
+async def fetLama(chara: str, prompt1: str):
     # Initialize the llamaapi with your api_token
     llama = LlamaAPI("LL-2FxqtbBbbOS82FwtvY0awiWv7jTnr1BXkFjDkfZTZEluJRJUZgZSCfK6gtCebXig")
 
     # Define your API request
-    api_request_json = request.json()
+    api_request_json = {
+        "messages": [
+            {"role": "user", "content": "act as "+ chara + " and answer the following question in english under 50 words. " + prompt1},
+        ],
+        
+        "stream": False,
+        
+    }
  
     # Make your request and handle the response
-    
     response = llama.run(api_request_json)
-    jeso = response.json()
-    json_response1 = json.dumps(response.json())
+    json_response1 = json.dumps(response.json(), indent=2)
+    json_response2 = json.loads(json_response1)
+    
     # Extracting "content"
-    content1 = json_response1["choices"][0]["message"]["content"]
+    content1 = json_response2["choices"][0]["message"]["content"]
     
     return {
         content1
     
     
     }
-
 
